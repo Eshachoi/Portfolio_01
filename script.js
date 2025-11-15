5caddb1e7788a509a61fe3d804f3b8b7c19db5df
Eshachoi <eshachoi@naver.com>
2025-11-15 21:42:01 +0900

    수정중
    수정

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ const navMenu = document.querySelector('.nav_menu');
 menuBtn.addEventListener('click',()=>{
     navMenu.classList.toggle("active");
 });
+
+//process
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const tabs = document.querySelectorAll(".tab");
+  const contents = document.querySelectorAll(".tab_content");
+
+  tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+      const target = tab.getAttribute("data-target");
+
+      contents.forEach(c => c.classList.remove("active"));
+      tabs.forEach(t => t.classList.remove("active"));   
+
+      tab.classList.add("active");
+
+      const selected = document.getElementById(target);
+      selected.classList.add("active");
+    });
+  });
+
+});
